c6601b666f11cbcd8872f7ae577471f9a71222c3
ntcababp <dihfahsihm@gmail.com>
2023-06-27 20:03:47 +0300

    connect to database

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -4,20 +4,19 @@ $host = 'localhost';
 $user = 'root';
 $password = 'MugoYA23?';
 $database = 'php_users';
+$dsn = "mysql:host=$host;dbname=$database";
 
 try {
-    // Create a new PDO instance
-    $dsn = "mysql:host=$host;dbname=$database";
-    $conn = new PDO($dsn, $user, $password);
+    $pdo = new PDO($dsn, $user, $password);
 
     // Set error mode to exception
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
     // Connection successful
     echo "Connected successfully";
 
-    // Close the connection
-    $conn = null;
+    // You can now execute SQL statements or perform other database operations using $conn
+
 } catch (PDOException $e) {
     // Connection failed
     echo "Connection failed: " . $e->getMessage();
--- index.php
+++ index.php
@@ -1,2 +1,23 @@
-<?php
-echo "Hello world!";
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title> Index Page</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="">
+    </head>
+    <body>
+        <h3>Signup<h3>
+        <form action="includes/formhandler.inc.php" method="post">
+            <input type="text" name="username" placeholder="Username">
+            <input type="password" name="pwd" placeholder="Password">
+            <input type="text" name="email" placeholder="E-mail">
+            <button>Submit</button>
+
+        </form>
+        
+        <script src="" async defer></script>
+    </body>
+</html>
