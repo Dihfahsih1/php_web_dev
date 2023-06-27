e9fef81e40a196eedba4a5871bab864ebcecc18b
Dihfahsih1 <dihfahsihm@gmail.com>
2023-06-27 22:35:59 +0300

    add some styling

--- index.php
+++ index.php
@@ -7,17 +7,65 @@
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="">
+        <style type="text/css">
+            .row {
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                height: 100vh;
+                }
+
+                .centered-div {
+                max-width: 400px;
+                padding: 20px;
+                border: 1px solid #ccc;
+                border-radius: 4px;
+                background-color: #f8f8f8;
+                }
+
+                .centered-div h3 {
+                text-align: center;
+                }
+
+                .centered-div form {
+                margin-top: 20px;
+                }
+
+                .centered-div input,
+                .centered-div button {
+                display: block;
+                width: 100%;
+                padding: 10px;
+                margin-bottom: 10px;
+                border: 1px solid #ccc;
+                border-radius: 4px;
+                }
+
+                .centered-div button {
+                background-color: #4CAF50;
+                color: white;
+                cursor: pointer;
+                }
+
+                .centered-div button:hover {
+                background-color: #45a049;
+                }
+
+        </style>
     </head>
     <body>
-        <h3>Signup<h3>
-        <form action="includes/formhandler.inc.php" method="post">
-            <input type="text" name="username" placeholder="Username">
-            <input type="password" name="pwd" placeholder="Password">
-            <input type="text" name="email" placeholder="E-mail">
-            <button>Submit</button>
+        <div class="row">
+            <div class="centered-div">
+                <h3>Signup</h3>
+                <form action="includes/formhandler.inc.php" method="post">
+                <input type="text" name="username" placeholder="Username">
+                <input type="password" name="pwd" placeholder="Password">
+                <input type="text" name="email" placeholder="E-mail">
+                <button type="submit">Submit</button>
+                </form>
+            </div>
+        </div>
 
-        </form>
-        
         <script src="" async defer></script>
     </body>
 </html>
