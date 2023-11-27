3659c85ad73f430e076c70ce47573069d40a593d
Dihfahsih1 <dihfahsihm@gmail.com>
2023-11-27 23:10:58 +0300

    submit images into the database

--- includes/process_registration.php
+++ includes/process_registration.php
@@ -4,26 +4,34 @@ ini_set('display_errors', 'On');
 
 require_once "dbconnect.php";
 
-$query = "INSERT INTO registration (firstName, secondName, email, telephone, course) VALUES (?, ?, ?, ?, ?)";
+$query = "INSERT INTO registration (firstName, secondName, email, telephone, course, avatar) VALUES (?, ?, ?, ?, ?, ?)";
 
 $stmt = mysqli_prepare($connect, $query);
 if (!$stmt) {
     die("Prepare failed: " . mysqli_error($connect));
 }
 
-mysqli_stmt_bind_param($stmt, "ssssss", $fName, $sName, $email, $tel, $course, $avatarPath);
+// Use file_get_contents to read the image file
+$avatarPath = $_FILES["avatar"]["tmp_name"];
+$avatarData = file_get_contents($avatarPath);
+
+// Bind parameters
+mysqli_stmt_bind_param($stmt, "ssssss", $fName, $sName, $email, $tel, $course, $avatarData);
+
+// Get other form data
 $fName = $_POST["firstName"];
 $sName = $_POST["secondName"];
 $email = $_POST["email"];
 $tel = $_POST["telephone"];
 $course = $_POST["course"];
-$avatarPath = $_POST["avatar"];
+
 if (mysqli_stmt_execute($stmt)) {
-    // header("Location: ../display_data.php?success=true");
-    echo "Data inserted successfully.";
+    header("Location: ../display_data.php?success=true");
+    // echo "Data inserted successfully.";
 } else {
     die("Execute failed: " . mysqli_error($connect));
 }
+
 mysqli_stmt_close($stmt);
 mysqli_close($connect);
 ?>
