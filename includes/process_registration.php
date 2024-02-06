0922e0c9f326551f017acc82a1bd93eee697bfcb
Dihfahsih1 <dihfahsihm@gmail.com>
2024-02-06 23:14:39 +0300

    more data

--- includes/process_registration.php
+++ includes/process_registration.php
@@ -14,23 +14,22 @@ if (!$stmt) {
 $avatarPath = $_FILES["avatar"]["tmp_name"];
 $avatarData = file_get_contents($avatarPath);
 
-// Bind parameters
-mysqli_stmt_bind_param($stmt, "ssssss", $fName, $sName, $email, $tel, $course, $avatarData);
-
 // Get other form data
 $fName = $_POST["firstName"];
 $sName = $_POST["secondName"];
 $email = $_POST["email"];
 $tel = $_POST["telephone"];
 $course = $_POST["course"];
+// Bind parameters
+mysqli_stmt_bind_param($stmt, "ssssss", $fName, $sName, $email, $tel, $course, $avatarData);
 
 if (mysqli_stmt_execute($stmt)) {
-    header("Location: ../display_data.php?success=true");
-    // echo "Data inserted successfully.";
-} else {
+    // header("Location: ../display_data.php?success=true");
+    echo "Data inserted successfully.";
+} 
+else {
     die("Execute failed: " . mysqli_error($connect));
 }
 
-mysqli_stmt_close($stmt);
-mysqli_close($connect);
+
 ?>
